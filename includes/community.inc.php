161dce9b378b01e11e48cd0028f51d7a7f0be278
Javier Pastor <jpastor@cerebelum.net>
2026-02-08 15:45:18 +0100

    fix community.inc.php
    syntax error, unexpected token "=", expecting variable in /opt/observium/includes/community.inc.php:5

--- includes/community.inc.php
+++ includes/community.inc.php
@@ -2,9 +2,9 @@
 
 // stub functions for not exist features
 function cache_groups() { return []; }
-function get_type_groups( = NULL,  = TRUE) { return []; }
-function get_groups_by_type( = NULL) { return []; }
-function get_group_entities(,  = '') { return []; }
-function get_group_entities_array(,  = '') { return []; }
+function get_type_groups( $var1 = NULL, $var2 = TRUE) { return []; }
+function get_groups_by_type( $var1 = NULL) { return []; }
+function get_group_entities($var1, $var2= '') { return []; }
+function get_group_entities_array( $var1, $var2 = '') { return []; }
 
 // EOF
